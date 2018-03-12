c00e20135382aa50b2e16192647f42306e010406
Allenknight <750959277@qq.com>
2018-03-12 16:57:06 +0800

    file provider

--- app/src/main/java/hsj/whatever/com/selfviewdemo/MainActivity.java
+++ app/src/main/java/hsj/whatever/com/selfviewdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package hsj.whatever.com.selfviewdemo;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -66,10 +68,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public File getFiles(String aName){
+        //任何时候都不要硬编码文件路径，可能存在安全隐患 也可能出现适配问题
         File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), aName);
         if(!file.mkdirs()){
             Log.e( "directory", "Directory not created");
         }
         return file;
+    }
+
+    //检查外部存储，必须检查外部存储的可用性
+    //读写检查
+    public boolean isExternalStorageWriteable(){
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            return true;
+        }
+        return false;
+    }
+
+    //只读检查
+    public boolean isExternalStorageReadable(){
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)){
+            return true;
+        }
+        return false;
+    }
+
+    public void updateSettingAsync(){
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("", "");
+        //apply在频繁存储的情况下 比commit性能要好 commit是直接存磁盘中 apply是先存内存再存磁盘  如果需要立刻使用存储数据的话 还是用commit
+        editor.apply();
+
+    }
+
+    public void updateSetting(){
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if(!editor.commit()){
+            Log.e("MainActivity", "Failed to commit setting changes");
+        }
     }
 }
